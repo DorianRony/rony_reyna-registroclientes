5599b73c9144319d8fdb021e527f312d4eb7bfda
Rony Reyna <36305590+DorianRony@users.noreply.github.com>
2023-08-16 23:13:59 -0500

    Initial commit

---
+++ src/test/java/ec/r2develop/registroclientes/RegistroclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.r2develop.registroclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
